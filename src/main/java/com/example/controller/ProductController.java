327aa6976eeee574fbc3a01ba332541f9df776d8
King Tran <king.tran@zuhlke.com>
2022-08-17 10:08:33 +0700

    Add dependency by internal module

--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -1,19 +1,19 @@
 package com.example.controller;
 
 import com.example.Product;
-import com.kawaii.service.proto.product.ProductServiceGrpc;
+import com.kawaii.service.proto.product.AnotherHelloGrpcGrpc;
+import com.kawaii.service.proto.product.AnotherHelloReply;
+import com.kawaii.service.proto.product.AnotherHelloRequest;
 import io.grpc.stub.StreamObserver;
 import io.quarkus.grpc.GrpcService;
 import io.smallrye.common.annotation.Blocking;
-import kawaii.proto.common.Common;
-import kawaii.proto.product.service.Model;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 @GrpcService
-public class ProductController extends ProductServiceGrpc.ProductServiceImplBase {
+public class ProductController extends AnotherHelloGrpcGrpc.AnotherHelloGrpcImplBase {
 
     @Inject
     EntityManager em;
@@ -21,10 +21,11 @@ public class ProductController extends ProductServiceGrpc.ProductServiceImplBase
     @Override
     @Transactional
     @Blocking
-    public void getProduct(Common.UUID request, StreamObserver<Model.Product> responseObserver) {
+    public void sayHello(AnotherHelloRequest request, StreamObserver<AnotherHelloReply> responseObserver) {
         Product product = new Product();
         em.persist(product);
-        responseObserver.onNext(Model.Product.newBuilder().build());
+        responseObserver.onNext(AnotherHelloReply.newBuilder().build());
         responseObserver.onCompleted();
     }
+
 }
